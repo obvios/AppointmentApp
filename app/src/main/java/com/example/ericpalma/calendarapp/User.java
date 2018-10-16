1cf4f4db3e3a0d7a17c5cf0f020c30ea70d6e2f7
Eric Palma <palmae95@gmail.com>
2018-10-16 14:46:29 -0700

    added changePasswrd method

--- app/src/main/java/com/example/ericpalma/calendarapp/User.java
+++ app/src/main/java/com/example/ericpalma/calendarapp/User.java
@@ -7,9 +7,15 @@ public class User {
     String lastName;
     String password;
 
+    /*Initialize instance*/
     public User(String f, String l, String pass){
         this.firstName = f;
         this.lastName = l;
         this.password = pass;
     }
-}
+
+    public void changePassword(String p){
+        if(!p.equals(""))
+            this.password = p;
+    }
+}//end of User
