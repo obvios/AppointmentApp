edb74141f62fb423e59cc4d71f96fcbc0c6496ff
Obvios <palmae95@gmail.com>
2018-10-16 22:24:44 -0700

    added Appointments constructor

--- app/src/main/java/com/example/ericpalma/calendarapp/Appointments.java
+++ app/src/main/java/com/example/ericpalma/calendarapp/Appointments.java
@@ -6,4 +6,12 @@ public class Appointments {
     int month;
     int day;
     int hour;
+
+    /*Initialize instance*/
+    public Appointments(int name, int mon, int d, int hr){
+        this.appointmentName = name;
+        this.month = mon;
+        this.day = d;
+        this.hour = hr;
+    }
 }
