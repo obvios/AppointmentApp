76e09c839388538308b611962bf1158b660831f6
Obvios <palmae95@gmail.com>
2018-10-21 17:51:41 -0700

    added queries to update user account data

--- app/src/main/java/com/example/ericpalma/calendarapp/AccountsDao.java
+++ app/src/main/java/com/example/ericpalma/calendarapp/AccountsDao.java
@@ -11,12 +11,18 @@ public interface AccountsDao {
     @Insert
     void insertAll(Accounts ... userAccounts);
 
-    @Query("DELETE FROM Accounts")
-    void deleteAll();
-
     @Query("SELECT * FROM Appointments WHERE apptUserName LIKE :usrName")
     List<Appointments> getAccountAppointments(String usrName);
 
+    @Query("UPDATE Accounts SET calendarColor = :newColor where username = :usrName")
+    void changeCalColorPreference(String usrName ,String newColor);
+
+    @Query("UPDATE Accounts SET calendarType = :newType WHERE username = :usrName")
+    void changeCalTypePreference(String usrName , String newType);
+
     @Delete
     void deleteAccount(Accounts userAccount1);
+
+    @Query("DELETE FROM Accounts")
+    void deleteAllAccounts();
 }
