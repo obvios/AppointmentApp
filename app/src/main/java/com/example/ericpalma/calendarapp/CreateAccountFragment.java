23cead667bd2ce8e004ad3a025ca08d0922d2c99
Eric Palma <palmae95@gmail.com>
2018-10-24 20:44:23 -0700

    process input from create account

--- app/src/main/java/com/example/ericpalma/calendarapp/CreateAccountFragment.java
+++ app/src/main/java/com/example/ericpalma/calendarapp/CreateAccountFragment.java
@@ -3,9 +3,12 @@ package com.example.ericpalma.calendarapp;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 
 /**
@@ -13,6 +16,9 @@ import android.view.ViewGroup;
  */
 public class CreateAccountFragment extends Fragment {
 
+    /*needed to handle input*/
+    private Button createButton;
+    private EditText mEditText;
 
     public CreateAccountFragment() {
         // Required empty public constructor
@@ -21,8 +27,19 @@ public class CreateAccountFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_create_account, container, false);
+        View view = inflater.inflate(R.layout.fragment_create_account, container, false);
+        createButton = view.findViewById(R.id.createAccountButton);
+        mEditText = view.findViewById(R.id.createAccountFirstName);
+        createButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //process input
+                Log.d(android.content.ContentValues.TAG, mEditText.getText().toString());
+            }
+        });
+        return view;
     }
 
 }
