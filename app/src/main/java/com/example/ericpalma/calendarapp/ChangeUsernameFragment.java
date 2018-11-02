d427a9a01da1de9539991e619b69c04a8de6b750
Obvios <palmae95@gmail.com>
2018-11-02 13:52:32 -0700

    chnaging username is fully functiona

--- app/src/main/java/com/example/ericpalma/calendarapp/ChangeUsernameFragment.java
+++ app/src/main/java/com/example/ericpalma/calendarapp/ChangeUsernameFragment.java
@@ -1,5 +1,6 @@
 package com.example.ericpalma.calendarapp;
 
+import android.arch.lifecycle.ViewModelProviders;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
@@ -7,17 +8,20 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 
 /**
  * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link ChangeUsernameFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link ChangeUsernameFragment#newInstance} factory method to
- * create an instance of this fragment.
+
  */
-public class ChangeUsernameFragment extends Fragment {
+public class ChangeUsernameFragment extends Fragment implements View.OnClickListener {
+    private AppViewModel appViewModel;
+    private Button changeUsernameButton;
+    private EditText oldUsernameText;
+    private EditText newUsernameText;
+    private EditText passwordText;
 
     public ChangeUsernameFragment() {
         // Required empty public constructor
@@ -26,7 +30,20 @@ public class ChangeUsernameFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        appViewModel = ViewModelProviders.of(getActivity()).get(AppViewModel.class);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_change_username, container, false);
+        View view = inflater.inflate(R.layout.fragment_change_username, container, false);
+        changeUsernameButton = view.findViewById(R.id.changeUsernameButton);
+        changeUsernameButton.setOnClickListener(this);
+        oldUsernameText = view.findViewById(R.id.change_username_fragment_OldUsername);
+        newUsernameText = view.findViewById(R.id.change_username_fragment_NewUsername);
+        passwordText = view.findViewById(R.id.change_username_fragment_Password);
+
+        return view;
+    }
+
+    @Override
+    public void onClick(View view){
+        appViewModel.changeUsername(oldUsernameText.getText().toString(),newUsernameText.getText().toString(),passwordText.getText().toString());
     }
 }
