ecded7d7499e3767e40a715e36a574fc407f9ae1
Obvios <palmae95@gmail.com>
2018-11-09 12:49:52 -0800

    create password fragment

--- app/src/main/java/com/example/ericpalma/calendarapp/ChangePasswordFragment.java
+++ app/src/main/java/com/example/ericpalma/calendarapp/ChangePasswordFragment.java
@@ -1,8 +1,6 @@
 package com.example.ericpalma.calendarapp;
 
 import android.arch.lifecycle.ViewModelProviders;
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
---
+++ app/src/main/java/com/example/ericpalma/calendarapp/CreateAppointmentFragment.java
@@ -0,0 +1,16 @@
+package com.example.ericpalma.calendarapp;
+
+import android.arch.lifecycle.ViewModelProviders;
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class CreateAppointmentFragment extends Fragment {
+
+}
